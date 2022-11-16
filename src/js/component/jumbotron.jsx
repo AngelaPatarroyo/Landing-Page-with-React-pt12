241c43a401d45eb84e611f8247807e69c19c555b
melbynrojo <angela.patarroyo@hotmail.com>
2022-11-16 23:39:42 +0000

    en proceso

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <>
+      <div className="jumbotron container mt-5">
+        <h1 className="display-4">A Warm Welcome</h1>
+        <p className="lead">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. psa, ipsam,
+          eligendi, in quo sunt possimus non incidunt odit vero aliquid
+          similique quaerat nam nobis illo aspernatur vita fugiat numquam
+          repellat.
+        </p>
+        <hr className="my-4" />
+        
+        <a className="btn btn-primary btn-lg" href="#" role="button">
+          Call to Action
+        </a>
+      </div>
+    </>
+  );
+};
+export default Jumbotron;
